d1a6146d9ce0551deebc4afb2b24acb12d7d9668
mohamed elmhassani <mohamed_elmhassani@live.fr>
2023-02-17 17:33:18 +0100

    modified blade templates to use Layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('Layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/style.css">
-    <title> {{ $post->title }}</title>
-</head>
-
-<body>
-    <section>
-        <article>
-            <h1> {{ $post->title }} </h1>
-            <span>Published: {{ $post->date }} </span>
-            {!! $post->body !!}
-        </article>
-        <a href="/" class="go-home">Go Home</a>
-    </section>
-</body>
-
-</html>
+@section('content')
+    <article>
+        <h1> {{ $post->title }} </h1>
+        <span>Published: {{ $post->date }} </span>
+        {!! $post->body !!}
+    </article>
+    <a href="/" class="go-home">Go Home</a>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('Layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/app.js" defer></script>
-    <title>My Blog</title>
-</head>
-
-<body>
-    <section>
-
-        @foreach ($posts as $post)
-            <article>
-                <h1><a href="/post/{{ $post->slug }}"> {{ $post->title }}</a></h1>
-                <span class="publish">Published: {{ $post->date }}</span>
-                {{ $post->excerpt }}
-            </article>
-        @endforeach
-
-    </section>
-</body>
-
-</html>
+@section('content')
+    @foreach ($posts as $post)
+        <article>
+            <h1><a href="/post/{{ $post->slug }}"> {{ $post->title }}</a></h1>
+            <span class="publish">Published: {{ $post->date }}</span>
+            {{ $post->excerpt }}
+        </article>
+    @endforeach
+@endsection
